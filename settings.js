b17969fd7ac3b3562e00d4750a3418d73ccf7cbb
Nadunmr <133474808+Nadunmr@users.noreply.github.com>
2024-12-26 17:07:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GrQ13BJC#ti1Bl93VjY1555anVgFnjVwa8VDCdYj0yNzeehP3t5Q' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello👋, I am Nadu 🥰 alive now!!*" : process.env.ALIVE_MSG,
+ALIVE_IMG: process.env.ALIVE_IMG === undefined ? "https://drive.google.com/file/d/1-5A7rD632xFk8_8oXE--295NsGDOflPR/view?usp=drivesdk" : process.env.ALIVE_IMG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
